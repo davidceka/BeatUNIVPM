3dfae8b3aa02fbe3354fcaebef4818f4479d4f2a
David Ceka <dav.ceka@gmail.com>
2023-07-10 22:09:05 +0200

    Controlli e funzionalità main menu

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void LoadGameScene()
+    {
+        SceneManager.LoadScene("Game");
+        Time.timeScale = 1f;
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
